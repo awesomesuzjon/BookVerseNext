700f9a04f55108986af7fb2d33faa409336a1a5f
Sujan Bohara <sbohara@hamropatro.com>
2024-02-13 11:23:49 +0545

    feat -> private route implementation with token check

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,36 @@
+"use client"
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import {useState} from "react";
+import Link from "next/link";
+
 export default function ProfilePage() {
+    const router = useRouter()
+    const [data, setData] = useState("")
+    const logOut = async () => {
+        try {
+            const response = await axios.get('/api/users/logout')
+            localStorage.removeItem(('token'))
+            window.location.href = '/login'
+        } catch (error: any) {
+            console.log(error.message)
+        }
+    }
+
+    const getUserDetails = async () => {
+        const res = await axios('/api/users/me')
+        setData(res?.data?.data._id)
+    }
+
     return (
         <div>
             <h1>Profile Page</h1>
+            <h2>{data ? <Link href={`/profile/${data}`}>
+                User Id
+            </Link> : "No data"}</h2>
+            <button onClick={getUserDetails} className={"p-4 bg-green-700 mt-4 hover:bg-blue-700"}>Get User Details
+            </button>
+            <button onClick={logOut} className={"p-4 bg-blue-500 mt-4 hover:bg-blue-700"}>Log Out</button>
         </div>
     )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from 'next/server'
+
+// This function can be marked `async` if using `await` inside => (LOGIC)
+export function middleware(request: NextRequest) {
+    //get the pathname from the request
+    const path = request.nextUrl.pathname
+    const isPublicPath = path === '/login' || path === '/signup'
+    const token = request.cookies.get('token')?.value || ''
+
+    //if the path is public and there is token, redirect to login
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+
+}
+
+// See "Matching Paths" below to learn more => (MATCHER)
+export const config = {
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signup',
+    ],
+}
