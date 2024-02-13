420a45e41d72d210e337543d6dc4bf6f00bfdec3
Sujan Bohara <sbohara@hamropatro.com>
2024-02-13 11:25:22 +0545

    feat -> middleware and helper files add to getDataFromToken

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -2,6 +2,7 @@ import {NextRequest} from "next/server";
 import jwt from 'jsonwebtoken';
 
 
+
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get('token')?.value || ''
